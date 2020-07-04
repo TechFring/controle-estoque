ae7bc195343701cb0c09c13900a27641d494337d
TechFring <danilo030920@gmail.com>
2020-07-04 10:49:12 -0300

    realizei alguns ajustes na barra de navegação

--- frontend/js/events.js
+++ frontend/js/events.js
@@ -5,3 +5,10 @@ $(document).ready(() => {
 $('#navbar ul li div a').on('click', () => {
     $('.navbar-collapse').collapse('hide');
 });
+
+$('.dropdown-item').on('click', e => {
+    $('#navbar').find('.active').removeClass('active');
+    $('#navbar').find('.disabled').removeClass('disabled');
+    $(e.target).closest('.nav-item').addClass('active');
+    $(e.target).closest('.nav-item').find('.nav-link').addClass('disabled');
+});
